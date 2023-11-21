b4f2d1129f1b5ac66a8888ff8eb71c10c6703422
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-21 20:50:17 +0800

    gives error messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, session, jsonify
+from flask import Flask, request, render_template, redirect, session, jsonify, flash
 from flask_debugtoolbar import DebugToolbarExtension
 import urllib.request
 import json
@@ -11,8 +11,8 @@ debug = DebugToolbarExtension(app)
 API_ACCESS_KEY = 'c52fe9fb2cf7ab057cff11a586b02291'
 
 def api_json(request_url):
-    reesponse = urllib.request.urlopen(f'{request_url}')
-    res_body = reesponse.read()
+    response = urllib.request.urlopen(f'{request_url}')
+    res_body = response.read()
     return json.loads(res_body.decode("utf-8"))
 
 @app.route('/')
@@ -24,14 +24,35 @@ def currency_converter():
     from_currency = request.form['from-currency']
     to_currency = request.form['to-currency']
     amount = request.form['amount']
-    # TODO: check if currencies and amount are valid, if not flash message?
 
+    if not is_valid_currency(from_currency):
+        flash(f'{ from_currency } is not a valid currency code.')
+        return render_template('form.html', from_currency=from_currency, to_currency=to_currency, amount=amount)
+
+    if not is_valid_currency(to_currency):
+        flash(f'{ to_currency } is not a valid currency code.')
+        return render_template('form.html', from_currency=from_currency, to_currency=to_currency, amount=amount)
+
+    if not is_valid_amount(amount):
+        flash(f'{ amount } is not a valid amount.')
+        return render_template('form.html', from_currency=from_currency, to_currency=to_currency, amount=amount)
+    
     # Construct the API endpoint URL with the access key and query parameters
     data = api_json(f'http://api.exchangerate.host/convert?access_key={API_ACCESS_KEY}&format=1&from={from_currency}&to={to_currency}&amount={amount}')
 
-    
-    converted_amount = data['result']
+    converted_amount = round(data['result'], 2)
 
     return render_template('form.html', from_currency=from_currency, to_currency=to_currency, amount=amount, converted_amount=converted_amount)
 
+def is_valid_currency(currency):
+    data = api_json(f'http://api.exchangerate.host/list?access_key={API_ACCESS_KEY}')
+    if currency in data['currencies']:
+        return True
+    else:
+        return False
 
+def is_valid_amount(num):
+    if int(num) <= 0:
+        return False
+    else:
+        return True
